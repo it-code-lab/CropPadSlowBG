19210a70f25dfa1d02ec6d6a791df39524dc77cd
NumeroUno <numerounodeveloper@gmail.com>
2025-07-03 08:10:26 -0400

    fix

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def batch_process(input_folder='input', output_folder='output', bg_music_folder=
     for filename in os.listdir(input_folder):
         if filename.lower().endswith(".mp4"):
             input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, f"processed_{filename}")
+            output_path = os.path.join(output_folder, f"{filename}")
             bg_music = get_random_music(bg_music_folder)
 
             if bg_music:
